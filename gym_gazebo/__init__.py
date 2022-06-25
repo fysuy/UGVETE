1e536183b531b8a537d866a29a465a72fffdf33f
Sebastian Gonzalez <uy.sgonzalez@gmailc.com>
2022-06-25 14:35:07 -0300

    Cleaned up unused files from gym gazebo

---
+++ gym_gazebo/__init__.py
@@ -0,0 +1,14 @@
+import logging
+from gym.envs.registration import register
+
+logger = logging.getLogger(__name__)
+
+# Gazebo
+# ----------------------------------------
+
+# Turtlebot envs
+register(
+    id='GazeboMazeTurtlebotLidar-v0',
+    entry_point='gym_gazebo.envs.turtlebot:GazeboMazeTurtlebotLidarEnv',
+    # More arguments here
+)
---
+++ gym_gazebo/envs/turtlebot/__init__.py
@@ -0,0 +1,1 @@
+from gym_gazebo.envs.turtlebot.gazebo_maze_turtlebot_lidar import GazeboMazeTurtlebotLidarEnv
