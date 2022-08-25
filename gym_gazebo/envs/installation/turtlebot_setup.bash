e2cce00f07569fcc0f2195f5b498135286694a43
Sebastian Gonzalez <uy.sgonzalez@gmail.com>
2022-08-25 19:48:02 -0300

    Added sarsa and circuit files, removed unused docker file

--- gym_gazebo/envs/installation/turtlebot_setup.bash
+++ gym_gazebo/envs/installation/turtlebot_setup.bash
@@ -16,25 +16,10 @@ if [ -z "$GYM_GAZEBO_WORLD_MAZE" ]; then
 else
   bash -c 'sed "s,GYM_GAZEBO_WORLD_MAZE=[^;]*,'GYM_GAZEBO_WORLD_MAZE=`pwd`/../assets/worlds/maze.world'," -i ~/.bashrc'
 fi
-if [ -z "$GYM_GAZEBO_WORLD_CIRCUIT" ]; then
-  bash -c 'echo "export GYM_GAZEBO_WORLD_CIRCUIT="`pwd`/../assets/worlds/circuit.world >> ~/.bashrc'
-else
-  bash -c 'sed "s,GYM_GAZEBO_WORLD_CIRCUIT=[^;]*,'GYM_GAZEBO_WORLD_CIRCUIT=`pwd`/../assets/worlds/circuit.world'," -i ~/.bashrc'
-fi
 if [ -z "$GYM_GAZEBO_WORLD_CIRCUIT2" ]; then
   bash -c 'echo "export GYM_GAZEBO_WORLD_CIRCUIT2="`pwd`/../assets/worlds/circuit2.world >> ~/.bashrc'
 else
   bash -c 'sed "s,GYM_GAZEBO_WORLD_CIRCUIT2=[^;]*,'GYM_GAZEBO_WORLD_CIRCUIT2=`pwd`/../assets/worlds/circuit2.world'," -i ~/.bashrc'
-fi
-if [ -z "$GYM_GAZEBO_WORLD_CIRCUIT2C" ]; then
-  bash -c 'echo "export GYM_GAZEBO_WORLD_CIRCUIT2C="`pwd`/../assets/worlds/circuit2c.world >> ~/.bashrc'
-else
-  bash -c 'sed "s,GYM_GAZEBO_WORLD_CIRCUIT2C=[^;]*,'GYM_GAZEBO_WORLD_CIRCUIT2C=`pwd`/../assets/worlds/circuit2c.world'," -i ~/.bashrc'
-fi
-if [ -z "$GYM_GAZEBO_WORLD_ROUND" ]; then
-  bash -c 'echo "export GYM_GAZEBO_WORLD_ROUND="`pwd`/../assets/worlds/round.world >> ~/.bashrc'
-else
-  bash -c 'sed "s,GYM_GAZEBO_WORLD_ROUND=[^;]*,'GYM_GAZEBO_WORLD_ROUND=`pwd`/../assets/worlds/round.world'," -i ~/.bashrc'
 fi
 
 #copy altered urdf model
